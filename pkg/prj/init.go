e67320b8ddc337bd15fe89bfd8df759d01034524
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2020-02-27 00:58:47 -0500

    Implemented the express file structure

--- pkg/prj/init.go
+++ pkg/prj/init.go
@@ -8,6 +8,7 @@ import (
 	"github.com/MohamedBeydoun/atlas/pkg/tpl"
 )
 
+// Project is the structure holding project information
 type Project struct {
 	Name         string
 	AbsolutePath string
@@ -17,26 +18,17 @@ type Project struct {
 
 // Create creates a new project directory with with a conventional express-typescript file structure
 func (p *Project) Create() error {
-	// create root directory
 	if _, err := os.Stat(p.AbsolutePath); os.IsNotExist(err) {
 		fmt.Printf("Creating new application \"%v\" at %v\n", p.Name, p.AbsolutePath)
 		if err := os.Mkdir(p.AbsolutePath, 0754); err != nil {
 			return err
 		}
+	} else {
+		fmt.Printf("Application \"%s\" already exists at %v\n", p.Name, p.AbsolutePath)
+		os.Exit(0)
 	}
 
-	// create folders
-	folders := []string{"src", "test"}
-	for _, folder := range folders {
-		if _, err := os.Stat(fmt.Sprintf("%s/%s", p.AbsolutePath, folder)); os.IsNotExist(err) {
-			if err := os.Mkdir(fmt.Sprintf("%s/%s", p.AbsolutePath, folder), 0751); err != nil {
-				return err
-			}
-		}
-	}
-
-	// populate new folders
-	err := p.populateSrc()
+	err := p.createFolders([]string{"test"}, p.AbsolutePath, 0751, true, 1)
 	if err != nil {
 		return err
 	}
@@ -45,44 +37,122 @@ func (p *Project) Create() error {
 		return err
 	}
 
-	// create README.md
-	readmeFile, err := os.Create(fmt.Sprintf("%s/README.md", p.AbsolutePath))
+	err = p.createFolders([]string{"src"}, p.AbsolutePath, 0751, true, 1)
 	if err != nil {
 		return err
 	}
-	defer readmeFile.Close()
-
-	readmeTemplate := template.Must(template.New("readme").Parse(string(tpl.ReadmeTemplate())))
-	err = readmeTemplate.Execute(readmeFile, p)
+	err = p.populateSrc()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("\tcreated README.md")
-
-	// create .gitignore
-	gitignoreFile, err := os.Create(fmt.Sprintf("%s/.gitignore", p.AbsolutePath))
+	err = p.createFile("package.json", p.AbsolutePath, string(tpl.PackageJSONTemplate()), 1)
 	if err != nil {
 		return err
 	}
-	defer gitignoreFile.Close()
-
-	gitignoreTemplate := template.Must(template.New("gitignore").Parse(string(tpl.GitignoreTemplate())))
-	err = gitignoreTemplate.Execute(gitignoreFile, p)
+	err = p.createFile("tslint.json", p.AbsolutePath, string(tpl.TSLintTemplate()), 1)
 	if err != nil {
 		return err
 	}
-
-	fmt.Println("\tcreated .gitignore")
+	err = p.createFile("tsconfig.json", p.AbsolutePath, string(tpl.TSConfigTemplate()), 1)
+	if err != nil {
+		return err
+	}
+	err = p.createFile("README.md", p.AbsolutePath, string(tpl.ReadmeTemplate()), 1)
+	if err != nil {
+		return err
+	}
+	err = p.createFile(".gitignore", p.AbsolutePath, string(tpl.GitignoreTemplate()), 1)
+	if err != nil {
+		return err
+	}
 
 	fmt.Println("Done")
 	return nil
 }
 
 func (p *Project) populateSrc() error {
+	srcFolders := []string{"routes", "controllers", "interfaces", "middleware", "util", "database"}
+	err := p.createFolders(srcFolders, p.AbsolutePath+"/src", 0751, true, 2)
+	if err != nil {
+		return err
+	}
+
+	dbFolders := []string{"models", "interactions"}
+	err = p.createFolders(dbFolders, p.AbsolutePath+"/src/database", 0751, true, 3)
+	if err != nil {
+		return err
+	}
+
+	err = p.createFile("app.ts", p.AbsolutePath+"/src", string(tpl.AppTemplate()), 2)
+	if err != nil {
+		return err
+	}
+	err = p.createFile("server.ts", p.AbsolutePath+"/src", string(tpl.ServerTemplate()), 2)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (p *Project) populateTest() error {
+	testFolders := []string{"routes"}
+	err := p.createFolders(testFolders, p.AbsolutePath+"/test", 0751, true, 2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Project) createFolders(folders []string, path string, permissions os.FileMode, withKeep bool, level int) error {
+	for _, folder := range folders {
+		if _, err := os.Stat(fmt.Sprintf("%s/%s", path, folder)); os.IsNotExist(err) {
+			if err := os.Mkdir(fmt.Sprintf("%s/%s", path, folder), permissions); err != nil {
+				return err
+			}
+			if withKeep {
+				keepFolder(path + "/" + folder)
+			}
+
+			for i := 0; i < level; i++ {
+				fmt.Print("    ")
+			}
+			fmt.Printf("%s/\n", folder)
+		}
+	}
+
+	return nil
+}
+
+func (p *Project) createFile(name string, path string, templateString string, level int) error {
+	file, err := os.Create(fmt.Sprintf("%s/%s", path, name))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fileTemplate := template.Must(template.New(name).Parse(templateString))
+	err = fileTemplate.Execute(file, p)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < level; i++ {
+		fmt.Print("    ")
+	}
+	fmt.Printf("%s\n", name)
+
+	return nil
+}
+
+func keepFolder(path string) error {
+	keepFile, err := os.Create(fmt.Sprintf("%s/.keep", path))
+	if err != nil {
+		return err
+	}
+	defer keepFile.Close()
+
 	return nil
 }
--- pkg/tpl/init.go
+++ pkg/tpl/init.go
@@ -1,14 +1,197 @@
 package tpl
 
+// ReadmeTemplate ...
 func ReadmeTemplate() []byte {
 	return []byte(`# {{ .Name }}
 
 `)
 }
 
+// GitignoreTemplate ...
 func GitignoreTemplate() []byte {
 	return []byte(`node_modules
 .nyc_output
 dist/
+`)
+}
+
+// PackageJSONTemplate ...
+func PackageJSONTemplate() []byte {
+	return []byte(`{
+    "name": "{{ .Name }}",
+    "version": "1.0.0",
+    "description": "",
+    "main": "server.ts",
+    "scripts": {
+        "start": "nodemon --watch 'src/**/*.ts' --exec 'ts-node' src/server.ts",
+        "build": "tsc -p tsconfig.json",
+        "watch": "tsc -w",
+        "lint": "tslint -c tslint.json -p tsconfig.json"
+    },
+    "author": "",
+    "license": "ISC",
+    "devDependencies": {
+        "@babel/preset-typescript": "^7.3.3",
+        "nodemon": "^1.19.0",
+        "source-map-support": "^0.5.12",
+        "ts-node": "^8.1.0"
+    },
+    "dependencies": {
+        "@types/cors": "^2.8.6",
+        "@types/express": "^4.16.1",
+        "@types/mongodb": "^3.1.26",
+        "@types/mongoose": "^5.5.0",
+        "@types/node": "^11.13.10",
+        "body-parser": "^1.19.0",
+        "cors": "^2.8.5",
+        "express": "^4.16.4",
+        "mongoose": "^5.5.7",
+        "tslint": "^5.16.0",
+        "typescript": "^3.4.5",
+    }
+}
+`)
+}
+
+// TSLintTemplate ...
+func TSLintTemplate() []byte {
+	return []byte(`{
+    "rules": {
+        "class-name": true,
+        "comment-format": [
+            true,
+            "check-space"
+        ],
+        "indent": [
+            true,
+            "spaces"
+        ],
+        "one-line": [
+            true,
+            "check-open-brace",
+            "check-whitespace"
+        ],
+        "no-var-keyword": true,
+        "quotemark": [
+            true,
+            "double",
+            "avoid-escape"
+        ],
+        "semicolon": [
+            true,
+            "always",
+            "ignore-bound-class-methods"
+        ],
+        "whitespace": [
+            true,
+            "check-branch",
+            "check-decl",
+            "check-operator",
+            "check-module",
+            "check-separator",
+            "check-type"
+        ],
+        "typedef-whitespace": [
+            true,
+            {
+                "call-signature": "nospace",
+                "index-signature": "nospace",
+                "parameter": "nospace",
+                "property-declaration": "nospace",
+                "variable-declaration": "nospace"
+            },
+            {
+                "call-signature": "onespace",
+                "index-signature": "onespace",
+                "parameter": "onespace",
+                "property-declaration": "onespace",
+                "variable-declaration": "onespace"
+            }
+        ],
+        "no-internal-module": true,
+        "no-trailing-whitespace": true,
+        "prefer-const": true
+    }
+}
+`)
+}
+
+// TSConfigTemplate ...
+func TSConfigTemplate() []byte {
+	return []byte(`{
+    "compilerOptions": {
+        "allowSyntheticDefaultImports": true,
+        "sourceMap": true,
+        "esModuleInterop": true,
+        "module": "commonjs",
+        "baseUrl": "./src",
+        "rootDir": "src",
+        "outDir": "dist",
+        "resolveJsonModule": true,
+        "target": "es6",
+        "lib": [
+            "esnext",
+            "es2018",
+            "dom"
+        ]
+    },
+    "include": [
+        "src/**/*.ts"
+    ],
+    "exclude": [
+        "node_modules",
+    ]
+}
+`)
+}
+
+// AppTemplate ...
+func AppTemplate() []byte {
+	return []byte(`import express from "express";
+import { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+export const port: Number = parseInt(process.env.PORT) || {{ .Port }};
+const app: Application = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req: Request, res: Response) => {
+    res.status(404).send({
+        status: 404,
+        message: "Invalid route"
+    });
+});
+
+export { app };
+`)
+}
+
+// ServerTemplate ...
+func ServerTemplate() []byte {
+	return []byte(`import { app, port } from "./app";
+import mongoose from "mongoose";
+
+let dbUrl = "";
+
+(process.env.DB_URL)
+    ? dbUrl = process.env.DB_URL
+    : dbUrl = "{{ .DBURL }}";
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+mongoose.set("useCreateIndex", true);
+
+const server = app.listen(port, async () => {
+    logger.info("Server listening on port " + port");
+});
+
+export { server };
 `)
 }
